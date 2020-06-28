da3117216becbdeb3afe4945c554676bf9edf66a
“FearwareX” <smalltime153@gmail.com>
2020-06-28 21:41:48 +0800

    update

--- codewars/6ku/Replace With Alphabet Position.js
+++ codewars/6ku/Replace With Alphabet Position.js
@@ -1,6 +1,16 @@
 const alphabetPosition = (text) => {
-  const array = text.split('');
-  return array;
+  const obj = {};
+  const arr = [];
+  const lowCaseText = text.toLowerCase();
+  for (let i = 0; i < 26; i += 1) {
+    obj[String.fromCharCode(65 + i).toLowerCase()] = i + 1;
+  }
+  for (let j = 0; j < text.length; j += 1) {
+    if (obj[lowCaseText[j]]) {
+      arr.push(obj[lowCaseText[j]]);
+    }
+  }
+  return arr.join(' ');
 };
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock."));
