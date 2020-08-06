0bf692766f958f9289d73c5b9357d91f25bbe309
Rain <smalltime153@gmail.com>
2020-08-06 21:15:05 +0800

    :memo: upload

--- unsolved/5ku/Multi Line Task: Fizz Buzz.js
+++ unsolved/5ku/Multi Line Task: Fizz Buzz.js
@@ -1,13 +1,30 @@
-const f = (n) => {
-  if (n % 15 === 0) {
-    return 'fizzbuzz';
-  }
-  if (n % 5 === 0) {
-    return 'buzz';
-  }
-  if (n % 3 === 0) {
-    return 'fizz';
-  }
-  return n;
-};
+// const f = (n) => {
+//   if (n % 15 === 0) {
+//     return 'fizzbuzz';
+//   }
+//   if (n % 5 === 0) {
+//     return 'buzz';
+//   }
+//   if (n % 3 === 0) {
+//     return 'fizz';
+//   }
+//   return n;
+// };
+
+f=
+n=>
+(n%
+3?
+'':
+'f\
+iz\
+z')
++(n
+%5?
+'':
+'b\
+uz\
+z')
+||n;
+
 console.log(f(3));
