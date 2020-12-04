6b9d344d98da95b1a9afa7abfb4ad22466c88577
“FearwareX” <smalltime153@gmail.com>
2020-12-05 00:49:40 +0800

    🎨 update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: 'shiwangme'
+  extends: 'rain'
 };
--- Leetcode/twoSum.js
+++ Leetcode/twoSum.js
@@ -1,6 +1,7 @@
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
+/* eslint-disable no-unused-vars */
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = 0; j < nums.length; j += 1) {
       if (nums[i] + nums[j] === target && i !== j) {
         return [i, j];
       }
